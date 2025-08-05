10c7043eae11ed68121f23535fba83f784c0eddc
pavel <pahanklm01@gmail.com>
2025-08-05 15:37:22 +0300

    fix zoomed image and fix overlay

--- src/components/module/Overlay/Overlay.tsx
+++ src/components/module/Overlay/Overlay.tsx
@@ -12,7 +12,7 @@ export const Overlay = () => {
         <motion.div
           className='overlay overlay-active'
           initial={{ opacity: 0 }}
-          animate={{ opacity: 0.6 }}
+          animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.3 }}
         />
