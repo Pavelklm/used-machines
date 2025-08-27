31a395bc2362c62db476b28f1b8c44609ffad9f6
pavel <pahanklm01@gmail.com>
2025-08-27 12:09:38 +0300

    fix burger

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -64,7 +64,7 @@ const ProductPage = () => {
   }
 
   return (
-    <div className='container'>
+    <div className=''>
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
