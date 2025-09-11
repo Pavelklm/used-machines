931af44cd2a6cd932acda0410d23ef3dc0b0b95e
pavel <pahanklm01@gmail.com>
2025-09-11 11:14:52 +0300

    add orig contacts

--- src/constants/Contacts.ts
+++ src/constants/Contacts.ts
@@ -1,7 +1,7 @@
 export const CONTACTS = {
-  phone: '+38 (050) 123-45-67',
-  phoneNumber: '+380501234567',
-  email: 'example@example.com',
+  phone: '+38 (099) 125-80-11',
+  phoneNumber: '+380991258011',
+  email: 'secondtech.co@gmail.com',
 
   address: '123 Main St, City, Country',
 }
